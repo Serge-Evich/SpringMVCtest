adb8673abeb2037487b272bbad383c891de73278
Serge-Evich <frenkdrebin@ukr.net>
2015-01-21 22:12:45 +0200

    add CatalogDaoTest

---
+++ springMVCtest/src/test/java/com/somecompany/datastore/CatalogDaoTest.java
@@ -0,0 +1,15 @@
+package com.somecompany.datastore;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CatalogDaoTest {
+	@Before
+	public void setup() {
+		
+	}
+	@Test
+	public void testFindOne() {
+		
+	}
+}
