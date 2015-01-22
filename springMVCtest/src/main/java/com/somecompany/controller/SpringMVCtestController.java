7d2cf5e6391571d508aace2e752bea9e148d237e
Serge-Evich <frenkdrebin@ukr.net>
2015-01-22 23:56:50 +0200

    c

--- springMVCtest/src/main/java/com/somecompany/controller/SpringMVCtestController.java
+++ springMVCtest/src/main/java/com/somecompany/controller/SpringMVCtestController.java
@@ -3,9 +3,16 @@ package com.somecompany.controller;
 import com.somecompany.service.CatalogService;
 
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SpringMVCtestController {
-    CatalogService catalogService;
+    private CatalogService catalogService;
+    
+    @Autowired
+    public SpringMVCtestController(CatalogService catalogService) {
+    	this.catalogService = catalogService;
+    }
 }
